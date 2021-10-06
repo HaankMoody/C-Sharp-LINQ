900d82e8ffe6e88ba7c995add4c083eb68704864
TEC\LHO <linus.hohenhaus@tecalliance.net>
2021-10-06 12:59:32 +0200

    LINQ Aufgabe 6

--- LINQ Exercise/LINQ Exercise/Program.cs
+++ LINQ Exercise/LINQ Exercise/Program.cs
@@ -78,6 +78,17 @@ namespace LINQ_Exercise
             Console.ReadKey();
             Console.WriteLine();
 
+            //Aufgabe 6: Gebe die Zahl, das Auftreten der Zahl im Array,
+            //sowie die Multiplikation der Zahl mit der Zahl ihres Auftretens:
+
+            int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+            foreach(var num in nums.GroupBy(i => i))
+            {
+                Console.WriteLine($"{num.Key} => {num.Count()}; {num.Key * num.Count()}");
+            }
+            Console.ReadKey();
+            Console.WriteLine();
 
         }
     }
