29d932df134f67d1fcdc7bf31ba164b87bcee086
TEC\LHO <linus.hohenhaus@tecalliance.net>
2021-10-07 13:46:59 +0200

    LINQ Aufgabe 13, 16 und 17 fehlen noch

--- LINQ Exercise/LINQ Exercise/Program.cs
+++ LINQ Exercise/LINQ Exercise/Program.cs
@@ -9,8 +9,25 @@ namespace LINQ_Exercise
     {
         static void Main(string[] args)
         {
+            //Aufgabe1();
+            //Aufgabe2();
+            //Aufgabe3();
+            //Aufgabe4();
+            //Aufgabe5();
+            //Aufgabe6();
+            //Aufgabe7();
+            //Aufgabe8und9();
+            //Aufgabe10();
+            //Aufgabe11();
+            //Aufgabe12();
+            //Aufgabe13();
+            //Aufgabe14und15();
+            Aufgabe16und17();
+        }
+        static void Aufgabe1()
+        {
             //Aufgabe 1: Gebe aus einer Liste von Zahlen nur die Zahlen aus, welche durch 2 Teilbar sind.
-            /*int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             var numQuery = from num in numbers
                            where num % 2 == 0
@@ -22,7 +39,9 @@ namespace LINQ_Exercise
             }
             Console.ReadKey();
             Console.WriteLine();
-
+        }
+        static void Aufgabe2()
+        {
             //Aufagbe 2: Gebe aus einer Liste von Zahlen nur die ungeraden Werte aus:
             int[] numbers2 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
@@ -30,14 +49,16 @@ namespace LINQ_Exercise
                             where num % 2 != 0
                             select num;
 
-            foreach(int num in numQuery2)
+            foreach (int num in numQuery2)
             {
                 Console.WriteLine(num);
 
             }
             Console.ReadKey();
             Console.WriteLine();
-
+        }
+        static void Aufgabe3()
+        {
             //Aufgabe 3: Gebe für die angegebenen Zahlen,
             //die Zahl sowie die Quadratzahl aus, wobei die Quadratzah größer als 20 sein soll:
 
@@ -46,25 +67,29 @@ namespace LINQ_Exercise
             var numQuery3 = from num in arr1
                             where (num * num) > 20
                             select num;
-            foreach(var num in numQuery3)
+            foreach (var num in numQuery3)
             {
-                Console.WriteLine($"{num} {num*num}");
+                Console.WriteLine($"{num} {num * num}");
             }
             Console.ReadLine();
             Console.WriteLine();
-
+        }
+        static void Aufgabe4()
+        {
             //Aufgabe 4: Gehe eine Liste von zahlen durch,
             //und gebe die Zahl sowie die Häufigkeit, wie oft diese in der Liste auftritt:
 
             int[] arr2 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
-            foreach(var num in arr2.GroupBy(i => i))
+            foreach (var num in arr2.GroupBy(i => i))
             {
                 Console.WriteLine("{0} => {1}", num.Key, num.Count());
             }
             Console.ReadKey();
             Console.WriteLine();
-
+        }
+        static void Aufgabe5()
+        {
             //Aufgabe 5:Gebe die Häufigkeit jeden Buchstabens des angegebenen Strings aus.
             //Lasse den Nutzer ein eigenes Wort eingeben:
 
@@ -72,29 +97,33 @@ namespace LINQ_Exercise
             string wort = Console.ReadLine();
             string newwort = string.Join("", wort);
 
-            foreach(var letter in newwort.GroupBy(i => i))
+            foreach (var letter in newwort.GroupBy(i => i))
             {
                 Console.WriteLine("{0} => {1}", letter.Key, letter.Count());
             }
             Console.ReadKey();
             Console.WriteLine();
-
+        }
+        static void Aufgabe6()
+        {
             //Aufgabe 6: Gebe die Zahl, das Auftreten der Zahl im Array,
             //sowie die Multiplikation der Zahl mit der Zahl ihres Auftretens:
 
             int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
 
-            foreach(var num in nums.GroupBy(i => i))
+            foreach (var num in nums.GroupBy(i => i))
             {
                 Console.WriteLine($"{num.Key} => {num.Count()}; {num.Key * num.Count()}");
             }
             Console.ReadKey();
             Console.WriteLine();
-
+        }
+        static void Aufgabe7()
+        {
             //Aufgabe 7:Gebe die Städte aus, welche mit einem bestimmten Buchstaben beginnen,
             //sowie mit einem weiteren Buchstaben Enden. Gestalte es variabel.
 
-            string[] cities = new string[] {  "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", 
+            string[] cities = new string[] {  "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
                                                     "NEW DELHI", "AMSTERDAM", "ABUDHABI", "PARIS" };
             Console.WriteLine($"Gebe einen Anfangsbuchstaben ein:");
             string firstLetter = Console.ReadLine().ToUpper();
@@ -104,13 +133,15 @@ namespace LINQ_Exercise
             var Ausgabe1 = cities.Where(a => a.StartsWith(firstLetter)).Select(a => a).ToList();
             var Ausgabe2 = Ausgabe1.Where(a => a.EndsWith(lastLetter)).Select(a => a).ToList();
 
-            foreach(string s in Ausgabe2)
+            foreach (string s in Ausgabe2)
             {
                 Console.WriteLine(s);
             }
             Console.ReadKey();
             Console.ReadLine();
-
+        }
+        static void Aufgabe8und9()
+        {
             //Aufgabe 8: Lasse den Nutzer eine Zahl eingeben, wie viele Einträge er hinzufügen möchte. 
             //Frage anschließend nach den jeweiligen Werten, bis die Anzahl der Listelemente erreicht ist. 
             //Frage Anschließend nach einer Zahl und gebe nur die Elemente der Liste aus, welche größer als die Eingabe ist.
@@ -143,25 +174,25 @@ namespace LINQ_Exercise
 
             Console.ReadKey();
             Console.WriteLine();
-
             //Aufgabe 9: Frage den Nutzer wie viele Einträge er ausgeben möchte,
             //sortiere die Liste den Werten nach absteigend und
             //gebe anschließend die n Einträge aus die der Nutzer sehen möchte. (Verwende die .Take() - Methode)
 
-            Console.WriteLine($"Wie viele Einträge möchtest du ausgeben=?");
+            Console.WriteLine($"Wie viele Einträge möchtest du ausgeben?");
             int ausgabeAnzahlEintrag = Convert.ToInt32(Console.ReadLine());
 
 
             var output = anzahlEintraege.OrderByDescending(zahl => zahl).Take(ausgabeAnzahlEintrag).ToList();
 
-            foreach(var x in output)
+            foreach (var x in output)
             {
                 Console.WriteLine(x);
             }
             Console.ReadKey();
             Console.WriteLine();
-
-
+        }
+        static void Aufgabe10()
+        {
             //Aufgabe 10: Lasse den Nutzer einen Satz eingeben, in welchen er bestimmte Wörter in UPPER CASE schreibt. 
             //Gebe anschließend nur die Wörter aus welche komplett mit UPPERCASE geschrieben wurden.
 
@@ -174,21 +205,25 @@ namespace LINQ_Exercise
 
             Console.WriteLine($"Das sind deine Großgeschriebenen Wörter:");
 
-            foreach(var y in upperCase)
+            foreach (var y in upperCase)
             {
                 Console.WriteLine(y);
             }
-
+        }
+        static void Aufgabe11()
+        {
             //Aufgabe 11: Speichere die Werte des angegeben String[] in einem gesamten String mit einem Komma separiert.
             //Gebe diesen String anschließend aus.
-            
+
             string[] arr1 = new string[4] { "cat", "dog", "cow", "tiger" };
 
             string newstring = String.Join(", ", arr1.Select(s => s.ToString()));
 
-            Console.WriteLine(newstring);*/
+            Console.WriteLine(newstring);
+        }
 
-            //Aufgabe 12: 
+        static void Aufgabe12()
+        {
             List<Students> stulist = new List<Students>();
             stulist.Add(new Students { StuId = 1, StuName = "Joseph", GrPoint = 800 });
             stulist.Add(new Students { StuId = 2, StuName = "Alex", GrPoint = 458 });
@@ -201,34 +236,79 @@ namespace LINQ_Exercise
             stulist.Add(new Students { StuId = 9, StuName = "Nicolash", GrPoint = 597 });
             stulist.Add(new Students { StuId = 10, StuName = "Jenny", GrPoint = 750 });
 
-            Console.WriteLine($"Bitte nenne mir eine Zahl zwischen 1 und 6:");
-            int eingabe = Convert.ToInt32(Console.ReadLine());
+            var queryPoints = from student in stulist
+                              group student by student.GrPoint into newGroup
+                              orderby newGroup.Key descending
+                              select newGroup;
 
-            switch (eingabe)
+            List<int> points = new List<int>();
+            foreach (var student in queryPoints)
             {
-                case 1:
-                    var maxQuery = from points in stulist
-                                   where points.GrPoint > 800
-                                   select points;
-                    break;
-
-                case 2:
-                    var maxQuerry = from points in stulist
-                                    where points.GrPoint < 900 && points.GrPoint >= 800
-                                    select points;
-                    break;
-
-                case 3:
-                    var maxQuerry = from points in stulist
-                                    where points.GrPoint == 750
-                                    select points;
-                    break;
-
-                case 4:
-
-
-
+                points.Add(student.Key);
             }
+
+            Console.WriteLine($"Welche Maximale Punktzahl suchst du? 1, 2, 3, 4, 5, 6?");
+            int rang = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Das sind die Studenten mit den jeweiligen Punkten:");
+            foreach (Students s in stulist)
+            {
+                if (s.GrPoint == points[rang - 1])
+                {
+                    Console.WriteLine(s.StuName + " " + s.GrPoint + " Punkte");
+                }
+            }
+        }
+        /*static void Aufgabe13()
+        {
+            //Zähle die Anzahl der jeweiligen File-Extensions im gegebenen Array.
+            string[] arr1 = { "aaa.frx", "bbb.TXT", "xyz.dbf", "abc.pdf", "aaaa.PDF", "xyz.frt", "abc.xml", "ccc.txt", "zzz.txt" };
+
+
+
+
+            Console.WriteLine($"{count} File(s) with {extension} Extension")
+        }*/
+        static void Aufgabe14und15()
+        {
+            //Entferne ein Element mittels der Remove Methode indem du davor über FirstOrDefault das Element „o“ erst findest.
+            List<string> listOfString = new List<string>();
+            listOfString.Add("m");
+            listOfString.Add("n");
+            listOfString.Add("o");
+            listOfString.Add("p");
+            listOfString.Add("q");
+
+            var firstO = listOfString.FirstOrDefault(letter => letter == "o");
+
+            listOfString.Remove(firstO);
+
+            listOfString.Remove("p");
+
+            foreach(var x in listOfString)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        static void Aufgabe16und17()
+        {
+            //Verkette die zwei Arrays indem das erste Element der ersten Liste mit dem ersten Element der zweiten Liste(2.Element der 1.Liste mit 2.Element der 2.Liste ….) als String gespeichert wird. 
+            var set1 = new string[] { "X", "Y", "Z" };
+            var set2 = new int[] { 1, 2, 3 };
+
+            
+
+            //var result1 = set1.Aggregate((concat, name) => concat + name);
+            var result2 = string.Join("", set2).ToArray();
+
+            set1 = set1.Concat(result2).ToArray();
+
+            
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            
+
+
         }
     }
 }
